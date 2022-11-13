2eb6aec0b6d3a8da8fc26a9b3c0d994f6c18c27e
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-13 15:01:43 -0600

    In this commit: Removed the placeholder.json file. Instead used a data.js file with the same values except that it is already placed in a data variable. Saving me the time to have to write json parser functions.

---
+++ assets/data/data.js
@@ -0,0 +1,30 @@
+var data = 
+    [
+        {
+            "id": 0,
+            "Question": "What is ...",
+            "CorrectAnswer": "A",
+            "A": "This is the A Answer",
+            "B": "This is the B Answer",
+            "C": "This is the C Answer",
+            "D": "This is the D Answer"
+        },
+        {
+            "id": 1,
+            "Question": "Second Question",
+            "CorrectAnswer": "B",
+            "A": "2 - This is the A Answer",
+            "B": "2 - This is the B Answer",
+            "C": "2 - This is the C Answer",
+            "D": "2 - This is the D Answer"
+        },
+        {
+            "id": 2,
+            "Question": "Third Question",
+            "CorrectAnswer": "C",
+            "A": "3 - This is the A Answer",
+            "B": "3 - This is the B Answer",
+            "C": "3 - This is the C Answer",
+            "D": "3 - This is the D Answer"
+        },
+]
