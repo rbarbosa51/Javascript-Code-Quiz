a66e5e73cdd6d51b4b130b251552bdf1b673aca3
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-13 14:30:28 -0600

    In this commit: I hid the question form. Then, I modified the Start Game button to unhide the questions (To start the game)

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,19 @@
 var buttonAnswer = document.getElementById('buttonSubmit');
+var buttonStart = document.getElementById('buttonStart');
 
+function showQuestions() {
+    var qForm = document.querySelector('form');
+    qForm.classList.remove('hidden');
+}
+
+//This button starts the game
+buttonStart.addEventListener('click', () => {
+    console.log('Start Game');
+    showQuestions();
+});
+
+//
 buttonAnswer.addEventListener('click',  (e) => {
     e.preventDefault();
     console.log('Button Clicked');
-})
+});
