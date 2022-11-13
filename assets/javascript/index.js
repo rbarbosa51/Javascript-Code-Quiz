4d9d14efb61ddeaa6fed387c91e88a7dfeaffbde
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-13 15:25:13 -0600

    In this commit I deleted the data.js and made a data.json. It is a pure json file, that will require parsing. I found a tutorial on the internet (see references) that explains it. It only works if launched as a live server (cant just open in local browser). I get values and print them to the console.
    Since it works as a localhost, I am about to upload to Github Pages to see if I can get it to work "in real life".

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,13 @@
+
 var buttonAnswer = document.getElementById('buttonSubmit');
 var buttonStart = document.getElementById('buttonStart');
+
+function loadQuestions() {
+    //Only Works in Local Computer
+    //fetch('http://127.0.0.1:5500/assets/data/data.json').then((response) => response.json()).then((json) => console.log(json));
+    //Works in Github Pages
+    fetch('https://rbarbosa51.github.io/Javascript-Code-Quiz//assets/data/data.json').then((response) => response.json()).then((json) => console.log(json));
+}
 
 function showQuestions() {
     var qForm = document.querySelector('form');
@@ -10,6 +18,9 @@ function showQuestions() {
 buttonStart.addEventListener('click', () => {
     console.log('Start Game');
     showQuestions();
+    //console.log(data);
+    loadQuestions();
+    
 });
 
 //
