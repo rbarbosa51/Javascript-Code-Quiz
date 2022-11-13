7daa407d4c877600f7b85fcb2e16e59bb1696de3
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-13 15:54:16 -0600

    In this commit Once the start button is clicked the form gets populated with the question data from the json file. I am pushing this commit to Github to debug on Pages

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,12 +1,33 @@
 
 var buttonAnswer = document.getElementById('buttonSubmit');
 var buttonStart = document.getElementById('buttonStart');
+var questionID = document.getElementById('questionID');
+var questionText = document.getElementById('questionText');
+var QA = document.getElementById('QA');
+var QB = document.getElementById('QB');
+var QC = document.getElementById('QC');
+var QD = document.getElementById('QD');
 
-function loadQuestions() {
+//This simplifies my life, so I can change between debugging on GitHub Pages and localhost. I just uncomment what I need
+//var url = 'http://127.0.0.1:5500/assets/data/data.json';
+var url = 'https://rbarbosa51.github.io/Javascript-Code-Quiz/assets/data/data.json';
+
+//Global Variable pointing to the current question
+var currentQuestionID = 0;
+var data = 5;
+function loadQuestions(currentQuestionID) {
     //Only Works in Local Computer
-    //fetch('http://127.0.0.1:5500/assets/data/data.json').then((response) => response.json()).then((json) => console.log(json));
-    //Works in Github Pages
-    fetch('https://rbarbosa51.github.io/Javascript-Code-Quiz/assets/data/data.json').then((response) => response.json()).then((json) => console.log(json));
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+        questionID.innerText = (json[currentQuestionID].id + 1);
+        questionText.innerText = json[currentQuestionID].Question;
+        QA.innerText = json[currentQuestionID].A;
+        QB.innerText = json[currentQuestionID].B;
+        QC.innerText = json[currentQuestionID].C;
+        QD.innerText = json[currentQuestionID].D;
+    });
 }
 
 function showQuestions() {
@@ -19,8 +40,8 @@ buttonStart.addEventListener('click', () => {
     console.log('Start Game');
     showQuestions();
     //console.log(data);
-    loadQuestions();
-    
+    loadQuestions(currentQuestionID);
+    //console.log(data);
 });
 
 //
