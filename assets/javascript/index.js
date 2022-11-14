56023a04cd828a7a6af128ab3ffeebb5d0a867c1
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-14 14:24:35 -0600

    In this commit: I set the loadQuestions to get the next questions set or console.log if there are no further questions to fetch.  It still does not check of the answer is correct

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,7 @@
 //For the timer
 var currentTimer = document.getElementById('timer');
 //For the quiz 
+var quizLength = 0;
 var buttonAnswer = document.getElementById('buttonSubmit');
 var buttonStart = document.getElementById('buttonStart');
 var questionID = document.getElementById('questionID');
@@ -24,14 +25,16 @@ var url = 'https://rbarbosa51.github.io/Javascript-Code-Quiz/assets/data/data.js
 
 //Global Variable pointing to the current question
 var currentQuestionID = 0;
-var data = 5;
 
 function loadQuestions(currentQuestionID) {
+    console.log('loadQuestions');
     //Only Works in Local Computer (Live Server) or Github Pages
     fetch(url)
     .then((response) => response.json())
     .then((json) => {
         console.log(json);
+        console.log(json.length);
+        quizLength = json.length;
         questionID.innerText = (json[currentQuestionID].id + 1);
         questionText.innerText = json[currentQuestionID].Question;
         QA.innerText = json[currentQuestionID].A;
@@ -76,7 +79,17 @@ buttonStart.addEventListener('click', () => {
 buttonAnswer.addEventListener('click',  (e) => {
     e.preventDefault();
     console.log('Button Clicked');
-    //setTime(5);
+    //Check if correct answer if not punish
+    
+    //Load the next question if there are any questions left
+    //I need to add 1 to compensate for the fact that I have to add 1. 
+    if (currentQuestionID + 1 < quizLength) {
+        currentQuestionID = currentQuestionID + 1;
+        loadQuestions(currentQuestionID);
+    } else {
+        console.log('No more questions');
+    }
+    
     
 });
 
